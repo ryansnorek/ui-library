a1f0bb57c381eae73b20cfe0cd833d8458b6f1a6
Ryan Snorek <ryan.snorek@manamonitoring.com>
2025-03-30 11:53:27 -0400

    temp - moved scenario component and styles to App for testing

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,7 +1,55 @@
-import './App.css';
+import { CSSProperties, ReactNode } from "react";
+
+interface ScenarioProps {
+  message: string | ReactNode;
+  visible: boolean;
+  header?: string;
+  submitLabel?: string;
+  closeLabel?: string;
+  style?: CSSProperties;
+  icon?: string;
+  className: string;
+  'data-testid': string;
+  onClose?: () => void;
+  onSubmit: () => void;
+}
+
+function Scenario(props: ScenarioProps) {
+  const {
+    header,
+    message,
+    visible,
+    icon,
+    submitLabel,
+    closeLabel,
+    style,
+    className,
+    onClose,
+    onSubmit,
+    ...rest
+  } = props;
+
+  return (
+    <div
+      data-testid={rest['data-testid']}
+      role="dialog"
+      style={style}
+      className={`container ${className}`}
+    >
+      <header>
+        <div>{header ?? 'Confirmation'}</div>
+        <button>&times;</button>
+      </header>
+    </div>
+  );
+}
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Scenario header="confirm" />
+    </>
+  );
 }
 
 export default App;
