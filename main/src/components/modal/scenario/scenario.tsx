0412589f19f0f3eb11d519a40827624b873e20b7
Ryan Snorek <ryan.snorek@manamonitoring.com>
2025-03-30 11:48:00 -0400

    define scenario modal props and create header

--- main/src/components/modal/scenario/scenario.tsx
+++ main/src/components/modal/scenario/scenario.tsx
@@ -1,8 +1,49 @@
-import { ScenarioProps } from './scenario.types';
+// import { ScenarioProps } from './scenario.types';
 
+import { CSSProperties, ReactNode } from 'react';
 
-export function Scenario(_props: ScenarioProps) {
-  return <div>scenario modal</div>
+interface ScenarioProps {
+  message: string | ReactNode;
+  visible: boolean;
+  header?: string;
+  submitLabel?: string;
+  closeLabel?: string;
+  style?: CSSProperties;
+  icon?: string;
+  className: string;
+  'data-testid': string;
+  onClose?: () => void;
+  onSubmit: () => void;
+}
+
+export function Scenario(props: ScenarioProps) {
+  const {
+    header,
+    message,
+    visible,
+    icon,
+    submitLabel,
+    closeLabel,
+    style,
+    className,
+    onClose,
+    onSubmit,
+    ...rest
+  } = props;
+
+  return (
+    <div
+      data-testid={rest['data-testid']}
+      role="dialog"
+      style={style}
+      className={`container ${className}`}
+    >
+      <header>
+        <div>{header ?? 'Confirmation'}</div>
+        <button>&times;</button>
+      </header>
+    </div>
+  );
 }
 
 export default Scenario;
